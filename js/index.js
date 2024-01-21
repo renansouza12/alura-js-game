1092a55fbcd0b12b0c4df2dd566d3aa887f29bbe
RenanSouza <renansouza0122@gmail.com>
2024-01-21 14:36:16 -0300

    alura desafio js game

--- js/index.js
+++ js/index.js
@@ -1,52 +1,101 @@
+let chosenNumbers = [];
+
+const gameStart = document.querySelector(".game_start");
+const gameContent = document.querySelector(".game_content");
 const endGame = document.querySelector(".game_end");
+
+const btnStart = document.querySelector(".btnStart");
 const btnGuess = document.querySelector(".btn_chute");
 const btnsRestart = document.querySelectorAll('.btn_reiniciar');
+
 const messageContainer = document.querySelector(".message_warning");
 let guess = document.querySelector("input");
 const message = document.querySelector(".message");
 
-const limitNumber = 10;
-const randomNumber =  getRandomNumber(); // working in the random number
+let limitNumber = 100;
+let secretNumber = getRandomNumber();
+let attempts = 1;
 
-console.log(randomNumber);
+btnStart.addEventListener("click", () => startButton());
+btnGuess.addEventListener("click", ()=> guessButton());
 
-btnGuess.addEventListener("click",()=>{ 
-    const guessValue = guess.value;
-    
-    if(guessValue == randomNumber){
-        console.log("you right");
-        endGame.style.display ="flex";
-        gameContent.style.display = "none";
-    }else if(guessValue < randomNumber){
-        console.log("the number is bigger");
-        warningMessage('maior')
-    }else{
-        console.log("the number is lower");
-        warningMessage('menor')
-    }
-    resetInput();
+btnsRestart.forEach(btn => {
+    btn.addEventListener('click', () => restartGame());
 })
 
-btnsRestart.forEach(btn=>{
-    btn.addEventListener('click',() => restartGame());
-})
 
-function getRandomNumber(){
-    const getNumber = parseInt(Math.random() * limitNumber + 1)
+function startButton(){
+    gameStart.classList.add("active");
+    gameContent.style.display = "flex";
 }
 
-function warningMessage(text){
+function guessButton(){
+    const guessValue = guess.value;
+
+    if (guessValue == secretNumber) {
+        endGame.style.display = "flex";
+        gameContent.style.display = "none";
+        checkAttempts();
+    } else if (guessValue < secretNumber) {
+        warningMessage('maior');
+        displayMessage('bigger','smaller');
+    } else {
+        warningMessage('menor');
+        messageContainer.classList.add('smaller');
+        messageContainer.classList.remove('bigger');
+        displayMessage('smaller','bigger');
+    }
+    attempts ++;
+    resetInput();
+}
+
+
+function getRandomNumber() {
+    const randomNumber = Math.floor(Math.random() * limitNumber + 1);
+
+    if (chosenNumbers.length == limitNumber) {
+        chosenNumbers = [];
+    }
+
+    if (chosenNumbers.includes(randomNumber)) {
+        console.log('there is a same number');
+        return getRandomNumber();
+    } else {
+        chosenNumbers.push(randomNumber);
+        return randomNumber;
+    }
+}
+
+
+function warningMessage(text) {
     messageContainer.style.display = "flex";
     message.innerHTML = `O número é ${text.toUpperCase()}`;
-    messageContainer.classList.toggle('active');
+
 }
 
-function resetInput(){
+function resetInput() {
     guess.value = "";
 }
 
-function restartGame(){
+function restartGame() {
     resetInput();
-    gameStart.classList.remove('active');  
-    endGame.style.display ="none";
+    gameStart.classList.remove('active');
+    endGame.style.display = "none";
+    secretNumber = getRandomNumber();
+    messageContainer.style.display = "none";
+    attempts = 1;
+
+}
+
+function checkAttempts() {
+    const messageAttempt = document.querySelector(".attempts");
+    
+    let attemptWord = attempts > 1 ? 'tentativas': 'tentativa';
+
+    messageAttempt.innerHTML = `apenas ${attempts} ${attemptWord}`;
+
+}
+function displayMessage(addClass, removeClass){
+    messageContainer.classList.add(addClass);
+    messageContainer.classList.remove(removeClass);   
 }
--- js/main.js
+++
@@ -1,8 +0,0 @@
-const btnStart = document.querySelector(".btnStart");
-const gameStart = document.querySelector(".game_start");
-const gameContent = document.querySelector(".game_content");
-
-btnStart.addEventListener("click",()=>{
-    gameStart.classList.add("active");
-    gameContent.style.display = "flex";
-})
